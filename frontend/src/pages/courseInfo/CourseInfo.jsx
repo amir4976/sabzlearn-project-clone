3c87c05bf9bba7d62af487d9cdff70816a4216bf
amir4976 <amirhosianahmadi4976@gmail.com>
2023-02-21 23:14:25 +0330

    sabzlearn

---
+++ frontend/src/pages/courseInfo/CourseInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CourseInfo.css'
+function CourseInfo() {
+  return (
+    <div>CourseInfo</div>
+  )
+}
+
+export default CourseInfo
---
+++ frontend/src/pages/index/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.css'
+function Index() {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index
